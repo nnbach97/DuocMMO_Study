b38ae122f17f48b884eb8d3adbb5618866625e95
VietIS Bach <bach.ngongoc@vietis.com.vn>
2023-04-11 11:28:01 +0700

    Config webpack SCSS / CSS

--- Webpacks/src/index.js
+++ Webpacks/src/index.js
@@ -1,1 +1,3 @@
+import "./css/style.css";
+import "./scss/style.scss";
 console.log(11111111);
--- Webpacks/webpack.config.js
+++ Webpacks/webpack.config.js
@@ -1,13 +1,20 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: {
     app: "./src/index.js",
   },
   output: {
     filename: "script.min.js",
     path: path.resolve(__dirname, "htdocs"),
-    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss|css$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
   },
 };
