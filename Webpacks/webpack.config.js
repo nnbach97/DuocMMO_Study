8fb2e4b7d7cfb10e296c2225d5babfd7866677a9
VietIS Bach <bach.ngongoc@vietis.com.vn>
2023-04-11 15:22:45 +0700

    Thêm devtool webpack

--- Webpacks/webpack.config.js
+++ Webpacks/webpack.config.js
@@ -2,42 +2,47 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  mode: "production",
-  entry: {
-    app: "./src/index.js",
-  },
-  output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "htdocs"),
-    clean: true,
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css",
-    }),
-    new HtmlWebpackPlugin({
-      title: "Webpack app",
-      filename: "index.html",
-      template: "./src/tempalte.html",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss|css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-    ],
-  },
-  devServer: {
-    static: {
-      directory: "dist",
+module.exports = (env) => {
+  const isDevelopment = Boolean(env.development);
+  console.log("isDevelopment", isDevelopment);
+  return {
+    mode: isDevelopment ? "development" : "production",
+    entry: {
+      app: "./src/index.js",
     },
-    port: 6789,
-    open: true,
-    hot: true,
-    compress: true,
-    historyApiFallback: true,
-  },
+    output: {
+      filename: "[name].[contenthash].js",
+      path: path.resolve(__dirname, "htdocs"),
+      clean: true,
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "[name].[contenthash].css",
+      }),
+      new HtmlWebpackPlugin({
+        title: "Webpack app",
+        filename: "index.html",
+        template: "./src/tempalte.html",
+      }),
+    ],
+    devtool: isDevelopment ? "source-map" : false,
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss|css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        },
+      ],
+    },
+    devServer: {
+      static: {
+        directory: "dist",
+      },
+      port: 6789,
+      open: true,
+      hot: true,
+      compress: true,
+      historyApiFallback: true,
+    },
+  };
 };
